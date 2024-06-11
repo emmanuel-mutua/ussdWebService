584077f24f58aa48b69005773f0728074b9bab49
emmanuel-mutua <emmamulwa2020@gmail.com>
2024-06-11 09:50:33 +0300

    update Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM openjdk:17
 VOLUME /tmp
 EXPOSE 8080
-ARG JAR_FILE=target/*.jar
-COPY ${JAR_FILE} ussd.jar
+ARG JAR_FILE=ussdWebService-0.0.1-SNAPSHOT.jar
+COPY target/${JAR_FILE} ussd.jar
 ENTRYPOINT [ "java", "-jar","ussd.jar" ]
--- src/test/java/com/example/ussdWebService/UssdWebServiceApplicationTests.java
+++ src/test/java/com/example/ussdWebService/UssdWebServiceApplicationTests.java
@@ -1,13 +1,8 @@
 package com.example.ussdWebService;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class UssdWebServiceApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
